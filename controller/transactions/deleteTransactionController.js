0d957eb9373ddfbdb2dade436759cf3a696a16d4
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-02-21 14:35:11 +0200

    Used auth middleware

--- controller/transactions/deleteTransactionController.js
+++ controller/transactions/deleteTransactionController.js
@@ -1,8 +1,15 @@
+const { isValidObjectId } = require("mongoose");
+const { BadRequest } = require("http-errors");
+
 const { deleteTransaction } = require("../../service/transactions");
 
 const deleteTransactionController = async (req, res, next) => {
   const { transactionId } = req.params;
   const { _id } = req.user;
+  if (!isValidObjectId(transactionId))
+    throw BadRequest(
+      "Argument passed in must be a string of 12 bytes or a string of 24 hex characters or an integer"
+    );
 
   const transaction = await deleteTransaction(transactionId, _id);
   if (!transaction) return next();
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const register = require('../controller/auth/registrationController');
-const login = require('../controller/auth/loginController');
-const { logout } = require('../controller/auth/logoutController');
-const { tryCatchWrapper } = require('../helpers/tryCatchWrapper');
-const { authorize } = require('../middlewares/authorize');
-const { googleAuth } = require('../controller/auth/googleAuthControler');
-const { googleRedirect } = require('../controller/auth/googleRedirectController');
+const register = require("../controller/auth/registrationController");
+const login = require("../controller/auth/loginController");
+const { logout } = require("../controller/auth/logoutController");
+const { tryCatchWrapper } = require("../helpers/tryCatchWrapper");
+const { authorize } = require("../middlewares/authorize");
+const { googleAuth } = require("../controller/auth/googleAuthControler");
+const {
+  googleRedirect,
+} = require("../controller/auth/googleRedirectController");
 
 const authRouter = express.Router();
 
-authRouter.post('/users/register', tryCatchWrapper(register));
-authRouter.get('/users/login', tryCatchWrapper(login));
-authRouter.post('/users/logout', tryCatchWrapper(authorize), tryCatchWrapper(logout));
-authRouter.get('/google', tryCatchWrapper(googleAuth));
-authRouter.get('/google-redirect', tryCatchWrapper(googleRedirect));
+authRouter.post("/users/register", tryCatchWrapper(register));
+authRouter.post("/users/login", tryCatchWrapper(login));
+authRouter.post(
+  "/users/logout",
+  tryCatchWrapper(authorize),
+  tryCatchWrapper(logout)
+);
+authRouter.get("/google", tryCatchWrapper(googleAuth));
+authRouter.get("/google-redirect", tryCatchWrapper(googleRedirect));
 
 module.exports = authRouter;

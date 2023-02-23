4757432079f69632fcd16eed9c33bd2e2dc4dbe6
Nadiia Kotyk <99249549+Nandy7777@users.noreply.github.com>
2023-02-23 11:54:33 +0200

    додала токен в реєстрацію

--- controller/auth/registrationController.js
+++ controller/auth/registrationController.js
@@ -1,30 +1,36 @@
 const { User } = require('../../models/userModel');
 const bcrypt = require('bcrypt');
 const { Conflict, BadRequest } = require('http-errors');
+const jwt = require('jsonwebtoken');
 
 async function register(req, res, next) {
-    const { email, password } = req.body;
-    if(!email) throw BadRequest("email is required");
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    try { 
-        const savedUser = await User.create({
-          email,
-          password: hashedPassword,
-        });
-         res.status(201).json({
-           user: {
-             email: savedUser.email
-           },
-         });
-
-    } catch(error){
-        console.log('error while saving user', error.name, error.message)
-        if (error.message.includes("E11000 duplicate key error")) {
-            throw Conflict('Email in use!');
-        }
-        throw error;
+  const { email, password } = req.body;
+  if(!email) throw BadRequest("email is required");
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    const savedUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '24h',
+    });
+    await User.findByIdAndUpdate(savedUser._id, { token });
+    res.status(201).json({
+      token,
+      user: {
+        email: savedUser.email,
+        balance: savedUser.balance,
+      },
+    });
+  } catch (error) {
+    console.log('error while saving user', error.name, error.message)
+    if (error.message.includes("E11000 duplicate key error")) {
+      throw Conflict('Email in use!');
     }
+    throw error;
+  }
 };
  
 module.exports = { register }

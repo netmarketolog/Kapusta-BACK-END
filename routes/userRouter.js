84eab87bfbb859dd34149c17429da4eaccfa599c
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-02-26 01:10:29 +0200

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,18 +1,23 @@
-const express = require("express");
+const express = require('express');
 
 const { authorize } = require('../middlewares/authorize');
-const { tryCatchWrapper } = require("../helpers/tryCatchWrapper");
-const { updateBalance } = require("../controller/balance/updateBalanceController");
-const { current } = require("../controller/auth/currentUserController");
-
+const { tryCatchWrapper } = require('../helpers/tryCatchWrapper');
+const {
+  updateBalance,
+} = require('../controller/balance/updateBalanceController');
+const { current } = require('../controller/auth/currentUserController');
 
 const userRouter = express.Router();
 
-userRouter.patch("/update", tryCatchWrapper(authorize), tryCatchWrapper(updateBalance));
-userRouter.get(
-  '/current',
+userRouter.patch(
+  '/update',
   tryCatchWrapper(authorize),
-  tryCatchWrapper(current)
+  tryCatchWrapper(updateBalance)
 );
+// userRouter.get(
+//   '/current',
+//   tryCatchWrapper(authorize),
+//   tryCatchWrapper(current)
+// );
 
-module.exports =  userRouter ;
+module.exports = userRouter;

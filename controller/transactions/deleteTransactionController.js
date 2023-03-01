fb1ed7daa3becb7f1f1f50ca4b683720c37bda79
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2023-03-01 13:53:37 +0200

    fixed delete transaction and update balance

--- controller/transactions/deleteTransactionController.js
+++ controller/transactions/deleteTransactionController.js
@@ -2,10 +2,11 @@ const { isValidObjectId } = require('mongoose');
 const { BadRequest } = require('http-errors');
 
 const { deleteTransaction } = require('../../service/transactions');
+const { User } = require('../../models');
 
 const deleteTransactionController = async (req, res, next) => {
   const { transactionId } = req.params;
-  const { _id } = req.user;
+  const { _id, balance } = req.user;
   if (!isValidObjectId(transactionId))
     throw BadRequest(
       'Argument passed in must be a string of 12 bytes or a string of 24 hex characters or an integer'
@@ -13,6 +14,11 @@ const deleteTransactionController = async (req, res, next) => {
 
   const transaction = await deleteTransaction(transactionId, _id);
   if (!transaction) return next();
+  let newBalance;
+  transaction.operation === 'income'
+    ? (newBalance = balance - transaction.sum)
+    : (newBalance = balance + transaction.sum);
+  await User.findByIdAndUpdate(_id, { balance: newBalance });
 
   res.json({ id: transactionId });
 };
